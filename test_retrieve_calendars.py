3ef91b450584e5dce97f56b1a3d0756c8a274f45
Erick Ratamero <erickmartins@gmx.net>
2019-03-07 12:10:38 +0000

    code that interfaces with google API shouldn't be tested like this

--- test_retrieve_calendars.py
+++
@@ -1,8 +0,0 @@
-from retrieve_calendars import retrieve_calendars
-from create_service import create_service
-
-
-def test_list_calendars_not_empty():
-    service = create_service()
-    val = retrieve_calendars(service)
-    assert val != []
